19bcf9ee75409675592fb7d3d76151d51034ef2c
Molla1441 <mhasim1441@gmail.com>
2023-11-02 20:35:50 +0530

    task add section and cancel section updated

--- Home_page/home.js
+++ Home_page/home.js
@@ -2,10 +2,17 @@ let addSection = document.querySelector('.add_section')
 let outerAddSection = document.querySelector('.add-section-outer')
 let middleImage = document.querySelector('.middle-img')
 let cancelButton = document.querySelector('.cancel-button')
+
 addSection.addEventListener('click', function (e) {
     // console.log(e.target);
-    addSection.style.display = " none"
+    addSection.style.opacity = "0"
     outerAddSection.style.opacity = '1'
     middleImage.style.display = 'none'
+})
+
+cancelButton.addEventListener('click', function (e) {
+    outerAddSection.style.opacity = '0'
+    middleImage.style.display = 'flex'
+    addSection.style.opacity = '1'
 })
 
