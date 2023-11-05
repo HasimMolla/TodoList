c82dafa252b01c9c17fdec9f1f370eae10dbc921
Molla1441 <mhasim1441@gmail.com>
2023-11-05 21:51:38 +0530

    just add "add Task" in first field

--- Home_page/home.js
+++ Home_page/home.js
@@ -2,6 +2,13 @@ let addSection = document.querySelector('.add_section')
 let outerAddSection = document.querySelector('.add-section-outer')
 let middleImage = document.querySelector('.middle-img')
 let cancelButton = document.querySelector('.cancel-button')
+let inputTaskName = document.querySelector('#name')
+let addTaskButton = document.querySelector('.add_task_right')
+let innerAddTask = document.querySelector('.add-section_inner')
+
+inputTaskName.addEventListener('click', (e) => {
+    inputTaskName.style.border = "0.5px solid #80808041";
+})
 
 addSection.addEventListener('click', function (e) {
     // console.log(e.target);
@@ -16,3 +23,8 @@ cancelButton.addEventListener('click', function (e) {
     addSection.style.opacity = '1'
 })
 
+
+addTaskButton.addEventListener('click', function (e) {
+    addTaskButton.style.opacity = '0'
+    innerAddTask.style.opacity = '1'
+})
