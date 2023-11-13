6a69a9b7a1f198db569580f33a15d917bd49f4ac
Molla1441 <mhasim1441@gmail.com>
2023-11-13 21:01:18 +0530

    signUp functionlity completed

---
+++ signUp/script.js
@@ -0,0 +1,24 @@
+// const loader = document.querySelector('.loader-main')
+// setTimeout()
+
+const getPin = document.getElementById("enter_Pin")
+
+const signUpBtn = document.querySelector('.signup_btn')
+
+const confirmPin = document.getElementById('confirm_Pin')
+
+signUpBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (getPin.value != confirmPin.value) {
+        alert('PIN not matched :(')
+    }
+    if (getPin.value.length != 4 && confirmPin.value.length != 4) {
+       return alert("Enter your 4 digits PIN")
+    }
+    
+
+    localStorage.setItem('pin', getPin.value)
+    window.location.href = "/login"
+
+})
+
