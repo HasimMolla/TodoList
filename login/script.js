a02833d710a19a1159b4afcef367729d4dca8e03
Molla1441 <mhasim1441@gmail.com>
2023-11-13 21:23:27 +0530

    login functionlity Done

---
+++ login/script.js
@@ -0,0 +1,22 @@
+const getPin = document.getElementById('enter_Pin')
+
+const loginBtn = document.getElementById('login_btn')
+
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    // console.log(getPin.value);
+    console.log(JSON.stringify(getPin.value));
+    console.log(JSON.stringify(getPin.value).length - 2);
+
+    if ((JSON.stringify(getPin.value).length - 2) != 4) {
+        return alert('Enter a valid PIN ')
+    }
+
+    const localStgPin = localStorage.getItem('pin')
+    console.log(localStgPin);
+
+    if (localStgPin != getPin.value) {
+        return alert('Enter a valid PIN')
+    }
+    window.location.href = "/Home_page"
+})
